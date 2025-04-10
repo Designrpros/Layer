6588d69e034fb64068ab862e7cd68f1b5fe185dd
Vegar Lee Berentsen <designr.pros>
2025-04-10 11:11:34 +0200

    all files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,30 @@
-// app/layout.tsx
 import "./globals.css";
 import Toolbar from "../components/Toolbar";
 import ChatBot from "../components/ChatBot";
+import StyledComponentsRegistry from "./styled-components-registry";
 
-
-// Metadata for the application
 export const metadata = {
   title: "Layer - Learn Web Design & Development",
   description: "Master web design and development with VSCode, Next.js, and Styled Components using Layer.",
   icons: {
-    icon: "/favicon.ico", // Adjust path if needed
+    icon: "/favicon.ico",
   },
 };
 
-// Separate viewport export
 export const viewport = {
   width: "device-width",
   initialScale: 1.0,
 };
 
-// RootLayout component with TypeScript typing
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <Toolbar />
-        {children}
-        <ChatBot />
+        <StyledComponentsRegistry>
+          <Toolbar />
+          {children}
+          <ChatBot />
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
